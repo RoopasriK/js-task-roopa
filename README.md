527073dd8cac6bb536e5dabd4b53f9e63848f4b0
RoopasriK <roopasrivk@gmail.com>
2020-03-04 17:32:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-task-roopa
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-task-roopa)
---
+++ index.js
@@ -0,0 +1,13 @@
+let name=prompt("Enter the name:","Roopasri");
+let a=prompt("Enter the start slice");
+let b=prompt("Enter the end slice");
+if((a!=NaN) || (b!=NaN))
+{
+  alert("Enter a number");
+  let a=prompt("Enter the start slice");
+  let b=prompt("Enter the end slice");
+  alert(name.slice(parseInt(a),parseInt(b)));
+}
+  alert(name.slice(parseInt(a),parseInt(b)));
+
+
